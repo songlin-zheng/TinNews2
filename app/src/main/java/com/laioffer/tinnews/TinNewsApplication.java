4f139ee3dbd49677257a591a2f3edcb2f8215954
Songlin Zheng <slzheng@umich.edu>
2020-05-11 11:08:28 +0800

    First Commit

---
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -0,0 +1,23 @@
+package com.laioffer.tinnews;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.laioffer.tinnews.database.AppDatabase;
+
+public class TinNewsApplication extends Application {
+
+    private static AppDatabase database;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Gander.setGanderStorage(GanderIMDB.getInstance());
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tin_db").build();
+    }
+    public static AppDatabase getDatabase() {
+        return database;
+    }
+}
